2a6d5009344100210bd92e871239ebe74b67d9f4
Kim Petersson <kim.petersson301@gmail.com>
2026-04-15 17:58:53 +0200

    set up test fixture for easy mocking of dao and controller

--- backend/test/unit/test_getUserByEmail.py
+++ backend/test/unit/test_getUserByEmail.py
@@ -1,15 +1,20 @@
 # created empty test file
 from src.controllers.usercontroller import UserController
 from unittest.mock import MagicMock
+import pytest
 
 # next step is to create fixture to always setup mock_dao and the controller
+@pytest.fixture
+def test_setup():
+    mock_dao = MagicMock()
+    user_controller = UserController(mock_dao)
+    return mock_dao, user_controller
 
-def test_get_user_by_email_Valid():
+def test_get_user_by_email_Valid(test_setup):
     # arrange
     test_email = "hej@mail.com"
-    mock_dao = MagicMock()
+    mock_dao, user_controller = test_setup
     mock_dao.find.return_value = [{"email": test_email}]
-    user_controller = UserController(mock_dao)
     # act
     result = user_controller.get_user_by_email(test_email)
     print(result)
