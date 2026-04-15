48a3f52eb42e058752b85b92a17323c65ea3fb7f
Kim Petersson <kim.petersson301@gmail.com>
2026-04-15 17:38:27 +0200

    added first unittest

--- backend/test/unit/test_getUserByEmail.py
+++ backend/test/unit/test_getUserByEmail.py
@@ -1,1 +1,15 @@
 # created empty test file
+from src.controllers.usercontroller import UserController
+from unittest.mock import MagicMock
+
+def test_get_user_by_email_True():
+    # arrange
+    mock_service = MagicMock()
+    mock_service.find.return_value = [{"email": "hej@mail.com"}]
+    user_controller = UserController(mock_service)
+    # act
+    result = user_controller.get_user_by_email("hej@mail.com")
+    print(result)
+
+    # assert
+    assert result == {"email": "hej@mail.com"}
