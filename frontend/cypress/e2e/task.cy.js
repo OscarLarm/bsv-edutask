21b7535ab59ed3b4e889893c0101c7a337207e41
Oscar <118494885+oscar-larm@users.noreply.github.com>
2026-05-07 10:30:37 +0200

    Add fixture and logging in.

--- frontend/cypress/e2e/task.cy.js
+++ frontend/cypress/e2e/task.cy.js
@@ -2,10 +2,24 @@ describe("Interaction with todo-items", () => {
   let uid; // user id
   let name; // name of the user (firstName + ' ' + lastName)
   let email; // email of the user
+  let taskId; // task id
+  let title; // task title
+  let description; // task description
+  let url; // task url
+  let todos; // todos associated with task
 
   beforeEach(function () {
     // enter the main main page
     cy.visit("http://localhost:3000");
+
+    // detect a div which contains "Email Address", find the input and type (in a declarative way)
+    cy.contains("div", "Email Address").find("input[type=text]").type(email);
+    // alternative, imperative way of detecting that input field
+    //cy.get('.inputwrapper #email')
+    //    .type(email)
+
+    // submit the form on this page
+    cy.get("form").submit();
   });
 
   before(function () {
@@ -20,23 +34,35 @@ describe("Interaction with todo-items", () => {
         uid = response.body._id.$oid;
         name = user.firstName + " " + user.lastName;
         email = user.email;
+
+        // Create a task
+        cy.fixture("task.json").then((task) => {
+          cy.request({
+            method: "POST",
+            url: "http://localhost:5000/tasks/create",
+            form: true,
+            body: {...task, userid: uid}
+          }).then((response) => {
+            taskId = response.body[0]._id.$oid;
+            title = task.title;
+            description = task.description;
+            url = task.url;
+            todos = task.todos;
+          });
+        });
       });
     });
   });
 
-  it("login to the system with an existing account", () => {
-    // detect a div which contains "Email Address", find the input and type (in a declarative way)
-    cy.contains("div", "Email Address").find("input[type=text]").type(email);
-    // alternative, imperative way of detecting that input field
-    //cy.get('.inputwrapper #email')
-    //    .type(email)
+  
+  it('see tasks', () => {
+    cy.get('.title-overlay').should('contain.text', title);
+  })
 
-    // submit the form on this page
-    cy.get("form").submit();
-
-    // assert that the user is now logged in
-    cy.get("h1").should("contain.text", "Your tasks, " + name);
-  });
+  it('open detailed view', () => {
+    cy.get()
+  })
+  
 
   after(function () {
     // clean up by deleting the user from the database
